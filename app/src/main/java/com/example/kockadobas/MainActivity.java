1a7195668efe31f594a58f051c37c14e41136df7
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-10-14 16:54:17 +0200

    Add functionality to throw button

--- app/src/main/java/com/example/kockadobas/MainActivity.java
+++ app/src/main/java/com/example/kockadobas/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kockadobas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     private int diceOneValue;
     private int diceTwoValue;
+    private boolean isTwoDice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 setOneDice();
+                isTwoDice=false;
             }
         });
         twoDice.setOnClickListener(new View.OnClickListener() {
@@ -46,12 +49,21 @@ public class MainActivity extends AppCompatActivity {
                         0.5f
                 ));
                 dice2.setImageResource(R.drawable.kocka1);
+                dice2.setVisibility(View.VISIBLE);
+                isTwoDice=true;
             }
         });
         throwDice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                diceOneValue = (int) (Math.random() * 6) + 1;
+                diceTwoValue = (int) (Math.random() * 6) + 1;
+                if(isTwoDice){
+                    setDiceValue(diceOneValue, dice1);
+                    setDiceValue(diceTwoValue, dice2);
+                }else{
+                    setDiceValue(diceOneValue, dice1);
+                }
             }
         });
         reset.setOnClickListener(new View.OnClickListener() {
@@ -62,7 +74,31 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void setOneDice(){
+    private void setDiceValue(int diceValue, ImageView dice) {
+        switch (diceValue) {
+            case 1:
+                dice.setImageResource(R.drawable.kocka1);
+                break;
+            case 2:
+                dice.setImageResource(R.drawable.kocka2);
+                break;
+            case 3:
+                dice.setImageResource(R.drawable.kocka3);
+                break;
+            case 4:
+                dice.setImageResource(R.drawable.kocka4);
+                break;
+            case 5:
+                dice.setImageResource(R.drawable.kocka5);
+                break;
+            case 6:
+                dice.setImageResource(R.drawable.kocka6);
+                break;
+
+        }
+    }
+
+    private void setOneDice() {
         dice1.setLayoutParams(new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.WRAP_CONTENT,
                 LinearLayout.LayoutParams.MATCH_PARENT,
@@ -73,22 +109,24 @@ public class MainActivity extends AppCompatActivity {
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 0f
         ));
+        dice2.setVisibility(View.INVISIBLE);
     }
 
-    private void init(){
-        dice1=findViewById(R.id.k1);
-        dice2=findViewById(R.id.k2);
-        oneDice=findViewById(R.id.oneDice);
-        twoDice=findViewById(R.id.twoDice);
+    private void init() {
+        dice1 = findViewById(R.id.k1);
+        dice2 = findViewById(R.id.k2);
+        oneDice = findViewById(R.id.oneDice);
+        twoDice = findViewById(R.id.twoDice);
         throwDice = findViewById(R.id.throwDice);
         reset = findViewById(R.id.reset);
-        log=findViewById(R.id.log);
+        log = findViewById(R.id.log);
         dice1.setImageResource(R.drawable.kocka1);
 
         //1 dice in the middle by default
         setOneDice();
+        isTwoDice=false;
 
-        diceOneValue=0;
-        diceTwoValue=0;
+        diceOneValue = 0;
+        diceTwoValue = 0;
     }
 }
